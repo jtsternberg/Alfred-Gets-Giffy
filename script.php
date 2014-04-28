fc0e11db52f4dd8e000f4bd79d476c913e63d8bc
Justin Sternberg <justin@dsgnwrks.pro>
2014-04-28 10:38:13 -0400

    update script to reflect what is in the workflow

--- script.php
+++ script.php
@@ -1,42 +1,62 @@
 <?php
-// require_once('extension_utils.php');
-// $utils = new ExtensionUtils();
+require_once('extension_utils.php');
+$utils = new ExtensionUtils();
 
-$q = '{query}';
+$gif_dir = __DIR__ . '/gifs-cache/';
+$feed    = __DIR__ . '/feed-cache/feed.php';
+$day_ago = time() - 60*60*24;
+$q       = '{query}';
+// $q       = 'cat';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://jtsternberg.com/?json&gifs={$q}' );
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$head = curl_exec($ch);
-$http_info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
+if ( file_exists( $feed ) ) {
+	$modified_time = filemtime( $feed );
 
-die( '<xmp>'. print_r( $http_info, true ) .'</xmp>' );
+	if ( $modified_time < $day_ago ) {
+		cache_gif_feed( $feed );
+	}
 
-// $list = json_decode( $http_info );
+} else {
+	cache_gif_feed( $feed );
+}
 
-// $results = array();
+require_once( $feed );
 
-// foreach( $list['data'] as $emot => $img ) {
-// 	$name = basename( $img );
+$results = array();
+$counter = 0;
+$files_to_be_cached = array();
 
-// 	if ( !empty($q) &&
-// 		strpos( $emot, $q ) === false &&
-// 		strpos( $name, $q ) === false
-// 	) continue;
-// 	$results[] = array(
-// 		'title' => $name,
-// 		// 'icon' => 'filetype',
-// 		'icon' => $img,
-// 		'valid' => '',
-// 		'uid' => '',
-// 		'subtitle' => "Copy to clipboard",
-// 		'arg' => "($emot)"
-// 	);
-// }
+foreach( $list->data as $filename => $img ) {
 
-// die( '<xmp>'. print_r( $results, true ) .'</xmp>' );
+	// Filter out if a term was searched & json
+	if ( false === stripos( $filename, $q ) ) {
+		continue;
+	}
 
-// echo $utils->toXml( $results );
+	if ( 10 <= $counter ) {
+		break;
+	}
+
+	$file_location = $gif_dir . str_ireplace( '/', '-', $filename );
+
+	if ( ! file_exists( $file_location ) && isset( $img->thumb_src ) && $img->thumb_src ) {
+		$files_to_be_cached[ $img->thumb_src ] = $file_location;
+	}
+
+	$results[] = array(
+		'title'    => $img->name,
+		// 'icon'  => 'filetype',
+		// 'icon'     => 'fakefile.php?src='. $img->src,
+		'icon'     => $file_location,
+		'valid'    => '',
+		'uid'      => '',
+		'subtitle' => "Copy to clipboard",
+		'arg'      => $img->src,
+	);
+
+	$counter++;
+}
+
+echo $utils->toXml( $results );
+
+clean_cached_gifs( $gif_dir );
+cache_gif_thumbs( $files_to_be_cached );
